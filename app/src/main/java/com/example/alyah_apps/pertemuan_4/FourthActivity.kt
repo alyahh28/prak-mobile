0bc786400bf294ce04f27532a8d56b64b1e873e8
alyahh <alyah24si@mahasiswa.pcr.ac.id>
2026-04-14 16:47:46 +0700

    P5 FIX

--- app/src/main/java/com/example/alyah_apps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/alyah_apps/pertemuan_4/FourthActivity.kt
@@ -10,49 +10,40 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.alyah_apps.R
 import com.example.alyah_apps.databinding.ActivityFourthBinding
-import com.example.alyah_apps.databinding.ActivityThirdBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
-import kotlin.math.log
-
 
 class FourthActivity : AppCompatActivity() {
-    lateinit var binding: ActivityFourthBinding
+    private lateinit var binding: ActivityFourthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityFourthBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.btnKembali.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
 
+        // Setup Toolbar
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = "Activity Fourth"
+            setDisplayHomeAsUpEnabled(true)
+        }
 
-            setSupportActionBar(binding.toolbar)
-            supportActionBar?.apply {
-                title = "Activity Fifth"
-                subtitle = "Ini adalah subtitle"
-                setDisplayHomeAsUpEnabled(true)
-                setDisplayShowHomeEnabled(true)
-            }
-            val name = intent.getStringExtra("name")
-            val from = intent.getStringExtra("from")
-            val age = intent.getIntExtra("age", 0)
-            Log.i("Data Intent", "Nama: $name , Usia: $age, Asal: $from")
-            }
-
-        Log.e("onCreate", "FourthActivity dibuat pertama kali")
+        // Ambil Data dari Intent
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age", 0)
+        Log.i("Data Intent", "Nama: $name , Usia: $age, Asal: $from")
 
         binding.btnShowSnackbar.setOnClickListener {
             Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
-                .setAction("Tutup") {
-                    Log.e("Info Snackbar", "Snackbar ditutup")
-                }
+                .setAction("Tutup") { Log.e("Info Snackbar", "Snackbar ditutup") }
                 .show()
         }
 
@@ -60,40 +51,26 @@ class FourthActivity : AppCompatActivity() {
             MaterialAlertDialogBuilder(this)
                 .setTitle("Konfirmasi")
                 .setMessage("Apakah Anda yakin ingin melanjutkan?")
-                .setPositiveButton("Konfirm") { dialog, _ ->
-
+                .setPositiveButton("Ya") { dialog, _ ->
                     dialog.dismiss()
                     Log.e("Info Dialog", "Anda memilih Ya!")
                 }
-                .setNegativeButton("Kembali") { dialog, _ ->
+                .setNegativeButton("Tidak") { dialog, _ ->
                     dialog.dismiss()
-                    Log.e("Info Dialog", "Anda memilih Tidak!")
                 }
                 .show()
         }
 
         binding.btnKembali.setOnClickListener {
-            onDestroy()
+            finish() // Gunakan finish() untuk kembali, jangan onDestroy() langsung
         }
     }
-    override fun onStart() {
-        super.onStart()
-        Log.e("onStart", "onStart: {nama_activity} terlihat di layar")
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.e("onDestroy", "{nama_activity} dihapus dari stack")
-    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                onBackPressedDispatcher.onBackPressed()
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
         }
+        return super.onOptionsItemSelected(item)
     }
-    }
-
+}
